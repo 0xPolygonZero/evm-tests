5c5c6d61adadcf21347fe0dfcb720eb9b09c9553
cpu <bored@cpu.dev>
2022-10-12 13:20:52 -0700

    Remove stale comment and add flag description

--- eth_test_parser/src/arg_parsing.rs
+++ eth_test_parser/src/arg_parsing.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 pub(crate) struct ProgArgs {
-    // #[arg(short, long, default_value_t = false)]
     #[arg(short, long, default_value_t = false)]
+    /// Allow deserializing without fetching git remote
     pub no_fetch: bool,
 }
