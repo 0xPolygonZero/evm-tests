74f2010f4ac4555af060fe1d789b03afa990dd6c
Robin Salen <salenrobin@gmail.com>
2023-07-16 11:57:59 -0400

    Ignore failing tests from bad gas limit config

--- evm_test_runner/src/persistent_run_state.rs
+++ evm_test_runner/src/persistent_run_state.rs
@@ -91,6 +91,7 @@ impl From<Vec<SerializableRunEntry>> for TestRunEntries {
 #[derive(Copy, Clone, Debug, Deserialize, Default, Serialize)]
 pub(crate) enum PassState {
     Passed,
+    Ignored,
     Failed,
     #[default]
     NotRun,
@@ -100,6 +101,7 @@ impl From<TestStatus> for PassState {
     fn from(v: TestStatus) -> Self {
         match v {
             TestStatus::Passed => PassState::Passed,
+            TestStatus::Ignored => PassState::Ignored,
             TestStatus::EvmErr(_)
             | TestStatus::IncorrectAccountFinalState(_)
             | TestStatus::TimedOut => PassState::Failed,
