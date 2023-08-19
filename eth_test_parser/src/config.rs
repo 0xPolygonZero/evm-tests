ef6e6dbf941040dcc9dbdc99a31876a48c3fe526
Robin Salen <salenrobin@gmail.com>
2023-08-19 14:31:02 -0400

    Parse all tests

--- eth_test_parser/src/config.rs
+++ eth_test_parser/src/config.rs
@@ -1,3 +1,5 @@
 pub(crate) const ETH_TESTS_REPO_URL: &str = "https://github.com/ethereum/tests.git";
 pub(crate) const ETH_TESTS_REPO_LOCAL_PATH: &str = "eth_tests";
 pub(crate) const TEST_GROUPS: [&str; 1] = ["GeneralStateTests"];
+// The following subgroups contain subfolders unlike the other test folders.
+pub(crate) const SPECIAL_TEST_SUBGROUPS: [&str; 2] = ["Shanghai", "VMTests"];
