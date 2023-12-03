1c70094ff05e05a3d408809495f5909e9611408f
Robin Salen <salenrobin@gmail.com>
2023-12-03 14:51:35 -0500

    Add Cancun folder

--- eth_test_parser/src/config.rs
+++ eth_test_parser/src/config.rs
@@ -3,4 +3,4 @@ pub(crate) const ETH_TESTS_REPO_LOCAL_PATH: &str = "eth_tests";
 pub(crate) const GENERAL_GROUP: &str = "BlockchainTests";
 pub(crate) const TEST_GROUPS: [&str; 1] = ["GeneralStateTests"];
 // The following subgroups contain subfolders unlike the other test folders.
-pub(crate) const SPECIAL_TEST_SUBGROUPS: [&str; 2] = ["Shanghai", "VMTests"];
+pub(crate) const SPECIAL_TEST_SUBGROUPS: [&str; 3] = ["Cancun", "Shanghai", "VMTests"];
