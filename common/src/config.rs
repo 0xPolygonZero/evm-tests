94543434b401513e383dc34bf34bf196f3a48c03
Robin Salen <salenrobin@gmail.com>
2024-06-14 14:33:02 -0400

    Fix Shanghai tests parsing

--- common/src/config.rs
+++ common/src/config.rs
@@ -1,4 +1,4 @@
 pub const GENERATION_INPUTS_DEFAULT_OUTPUT_DIR: &str = "generation_inputs";
-pub const MAIN_TEST_DIR: &str = "BlockchainTests";
+pub const MAIN_TEST_DIR: &str = "Cancun/BlockchainTests";
 pub const MATIC_CHAIN_ID: u64 = 137;
 pub const ETHEREUM_CHAIN_ID: u64 = 1;
--- eth_test_parser/src/config.rs
+++ eth_test_parser/src/config.rs
@@ -1,6 +1,10 @@
-pub(crate) const ETH_TESTS_REPO_URL: &str = "https://github.com/ethereum/tests.git";
+use common::config::MAIN_TEST_DIR;
+
+// The PR <https://github.com/ethereum/tests/pull/1380> moved all test versions prior Cancun HF
+// to the `LegacyTests` folder instead.
+pub(crate) const ETH_TESTS_REPO_URL: &str = "https://github.com/ethereum/legacytests.git";
 pub(crate) const ETH_TESTS_REPO_LOCAL_PATH: &str = "eth_tests";
-pub(crate) const GENERAL_GROUP: &str = "BlockchainTests";
+pub(crate) const GENERAL_GROUP: &str = MAIN_TEST_DIR;
 pub(crate) const TEST_GROUPS: [&str; 1] = ["GeneralStateTests"];
 // The following subgroups contain subfolders unlike the other test folders.
-pub(crate) const SPECIAL_TEST_SUBGROUPS: [&str; 3] = ["Cancun", "Shanghai", "VMTests"];
+pub(crate) const SPECIAL_TEST_SUBGROUPS: [&str; 2] = ["Shanghai", "VMTests"];
