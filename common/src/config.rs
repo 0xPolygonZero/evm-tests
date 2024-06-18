b7c69b7a1450a6fa716c9273b810dea3f5a2d66c
Robin Salen <salenrobin@gmail.com>
2024-06-18 15:41:20 -0400

    Add comment

--- common/src/config.rs
+++ common/src/config.rs
@@ -1,4 +1,7 @@
 pub const GENERATION_INPUTS_DEFAULT_OUTPUT_DIR: &str = "generation_inputs";
+/// The source directory to look for tests to parse.
+/// We use the `BlockchainTests` subdirectory of the `Cancun` folder
+/// as it contains all hardfork variants up to this one.
 pub const MAIN_TEST_DIR: &str = "Cancun/BlockchainTests";
 pub const MATIC_CHAIN_ID: u64 = 137;
 pub const ETHEREUM_CHAIN_ID: u64 = 1;
